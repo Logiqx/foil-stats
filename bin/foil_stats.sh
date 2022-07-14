d9ede51563b539ed67775762692ffaf670340120
Logiqx <software@mikeg.me.uk>
2022-07-14 14:20:23 +0100

    Optimise PNG files

---
+++ bin/foil_stats.sh
@@ -0,0 +1,13 @@
+# Project Env
+. $(dirname $0)/env.sh
+
+# Refresh stats
+run_py_script foil_stats.py
+ERRVAL=$?
+
+# Compress Images
+if [[ $ERRVAL == 0 ]]
+then
+	find docs/img -name *.png -exec optipng -o2 {} \;
+	ERRVAL=$?
+fi
